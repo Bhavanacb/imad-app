02eda75ce2008ae036547bc7400182d07a044609
Bhavanacb <30979489+Bhavanacb@users.noreply.github.com>
2017-08-21 17:40:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one',function(req,res){
+   res.sendFile("article one is created and will be served here");   
+});
+
+app.get('article-two',function(req,res){
+   res.sendFile("article two is created and will be served here");   
+});
+
+app.get('article-three',function(req,res){
+   res.sendFile("article three is created and will be served here");   
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
