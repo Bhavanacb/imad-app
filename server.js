39a9507d187f1531a0487f568156419a7cb70994
Bhavanacb <30979489+Bhavanacb@users.noreply.github.com>
2017-08-24 21:06:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+    title: 'Article-one | Bhavana',
+    heading: 'Article-one',
+    date: '21 aug,2017',
+    content: `
+        <p>
+             I am now working on my web develpoing skills.
+         </p>
+         <p>
+             I know i should have started it before but atleast I am not giving up.
+         </p>`
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+   var htmlTemplate=`
+   <html>
+     <head>
+     <title>
+         ${title}
+     </title>
+     <meta name="viewport" contents="width=device-width,initial-scale=1" />
+      <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+     <body>
+     <div class="container">
+       <div>
+         <a href='/'>Home</a>
+      </div>
+      <hr/>
+      <h3>
+         ${heading}
+     </h3>
+     <div>
+        ${date} 
+     </div>
+     <div>
+         ${content}
+     </div>
+     </div>
+    </body>
+  </html>
+  ';
+  return htmlTemplate;
+} 
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+      res.send(createTemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
