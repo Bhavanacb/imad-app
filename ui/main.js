c7b83622d1b583cd46d83a2897cd46d15e01a6fd
Bhavanacb <30979489+Bhavanacb@users.noreply.github.com>
2017-08-27 15:27:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function () {
     request.send(null);
 };
 
-//submit name
+//Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
@@ -34,7 +34,7 @@ submit.onclick = function() {
     //Capture a list of names and render it as a list
     var names = ['name1','name2','name3'];
     var list = '';
-    for(var i=0;i<names.length;i++) {
+    for(var i=0; i<names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
